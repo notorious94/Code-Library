9d42dc3dd3a9ac71d227416cc9f826bd5c203063
MD. Istiad Hossain <akib94dhk@gmail.com>
2018-10-24 00:21:38 +0600

    Update Miller Rabin Primallity Testing.cpp

--- Number Theory/Miller Rabin Primallity Testing.cpp
+++ Number Theory/Miller Rabin Primallity Testing.cpp
@@ -1,24 +1,26 @@
 #include<bits/stdc++.h>
+#define pb push_back
+
 using namespace std;
 
 typedef long long ll;
 typedef unsigned long long ull;
 
-ull mulmod(ull a, ull b, ull c)
+ll mulmod(ll a, ll b, ll c)
 {
-	ull x = 0,y=a%c;
-	while(b>0)
+    ll x = 0, y = a % c;
+    while (b > 0)
     {
-		if(b&1) x = (x+y)%c;
-		y = (y<<1)%c;
-		b >>= 1;
-	}
-	return x;
+        if (b&1) x = (x + y) % c;
+        y = (y << 1) % c;
+        b >>= 1;
+    }
+    return x % c;
 }
 
-ull power(ull a, ull b, ull c)
+ll power(ll a, ll b, ll c)
 {
-	ull x = 1, y = a;
+	ll x = 1, y = a;
 	while(b>0)
     {
 		if(b&1) x = mulmod(x,y,c);
@@ -28,15 +30,34 @@ ull power(ull a, ull b, ull c)
 	return x;
 }
 
-bool Miller_Rabin(ull Prime,ull iter=1)
+bool witness(ll a,ll n)
 {
-    if(Prime<=1)            return false;
-    if(Prime==2||Prime==3)  return true;
-    if(Prime%2==0)          return false;
-    for(int i=1;i<=1;i++)
+    ll t = 0, u = n-1;
+    while(u%2==0)
     {
-        ull a = 2 + rand()%(Prime-4);
-        if(power(a,Prime-1,Prime)!=1)
+        u>>=1;
+        t++;
+    }
+    ll x[t+1];
+    x[0] = power(a,u,n);
+    for(int i=1;i<=t;i++)
+    {
+        x[i] = mulmod(x[i-1],x[i-1],n);
+        if(x[i]==1 && x[i-1]!=1 && x[i-1]!=n-1)
+            return true;
+    }
+    if(x[t]!=1)
+        return true;
+    return false;
+}
+
+bool miller_rabin(ll p,ll k=2)
+{
+    srand(time(NULL));
+    for(int i=1;i<=k;i++)
+    {
+        ll a = 2 + rand()%(p-4);
+        if(witness(a,p))
             return false;
     }
     return true;
@@ -44,14 +65,14 @@ bool Miller_Rabin(ull Prime,ull iter=1)
 
 int main()
 {
-    //freopen("in.txt","r",stdin);
-    //freopen("out.txt","w",stdout);
-    ll P,Test;
-    cin>>Test;
-    while(Test--)
+    //freopen("in.txt","r", stdin);
+    //freopen("out.txt","w", stdout);
+    ll p,t;
+    cin>>t;
+    while(t--)
     {
-        cin>>P;
-        if(Miller_Rabin(P))
+        scanf("%lld",&p);
+        if(miller_rabin(p))
             puts("YES");
         else
             puts("NO");
